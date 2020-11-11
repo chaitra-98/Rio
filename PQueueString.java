452dc7e8e63a94751ec344205863c8ce4170d40f
chaitra-98 <chaitras.xworkz.com>
2020-11-11 22:20:43 +0530

    Queue Assignment

---
+++ PQueueString.java
@@ -0,0 +1,29 @@
+package queue;
+
+import java.util.PriorityQueue;
+
+public class PQueueString {
+public static void main(String[] args) {
+		
+		PriorityQueue<String> ref = new PriorityQueue<String>();
+		ref.add("red");
+		ref.add("green");
+		ref.add("blue");
+		ref.add("black");
+		ref.add("white");
+		System.out.println(ref);
+		
+		for (String Str : ref) {
+			System.out.println(Str);
+		}
+		
+		System.out.println("First element : " + ref.peek() );
+		System.out.println("Remove element : " + ref.poll());
+		ref.element();
+		System.out.println(" element method : " + ref);
+		System.out.println("Using offer-Method : " + ref.offer("white"));
+		System.out.println(ref);
+	}
+
+
+}
